a4a26b7e6ecc3741d185c4ba1fa482c30cff348f
dilboz <dilbozsafarov@gmail.com>
2024-11-23 15:20:33 +0500

    feat: array task contains duplicate

---
+++ ContainsDuplicate/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ContainsDuplicate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+            Console.WriteLine(ContainsDuplicate(nums));
+        }
+
+        private static bool ContainsDuplicate(int[] nums) {
+            var set = new HashSet<int>();
+            for (var i = 0; i < nums.Length; i++) {
+                if (set.Contains(nums[i])) {
+                    return true;
+                }
+                set.Add(nums[i]);
+            }
+            return false;
+        }
+    }
+    
+}
