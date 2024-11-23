cf3648f54c58ec9fefe72b235f4f71171ad72cdf
dilboz <dilbozsafarov@gmail.com>
2024-11-23 16:15:55 +0500

    feat: concatination of array

---
+++ ConcatenationOfArray/Program.cs
@@ -0,0 +1,19 @@
+﻿public class Program
+{
+    public static void Main()
+    { 
+        Console.WriteLine(string.Join(",",ConcatenationOfArray([1, 2, 1])));
+        Console.WriteLine(string.Join(",", ConcatenationOfArray([1, 3, 2, 1])));
+    }
+    
+    private static int[] ConcatenationOfArray(int[] nums)
+    {
+        var result = new int[nums.Length * 2];
+        for (int i = 0; i < nums.Length; i++)
+        {
+            result[i] = nums[i];
+            result[i + nums.Length] = nums[i];
+        }
+        return result;
+    }
+}
