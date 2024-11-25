0d9541d1f786400b4867826b8acd44c48f627223
dilboz <dilbozsafarov@gmail.com>
2024-11-25 11:20:08 +0500

    feat: is subsequence

---
+++ IsSubsequence /Program.cs
@@ -0,0 +1,25 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine(IsSubsequence("abc", "ahbgdc"));
+        Console.WriteLine(IsSubsequence("axc", "ahbgdc"));
+    }
+
+    private static bool IsSubsequence(string s, string t)
+    {
+        var i = 0;
+        var j = 0;
+        while (i < s.Length && j < t.Length)
+        {
+            if (s[i] == t[j])
+            {
+                i++;
+            }
+
+            j++;
+        }
+
+        return i == s.Length;
+    }
+}
