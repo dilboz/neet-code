b648071aa8d143ff06c0874a7897d27248711b50
dilboz <dilbozsafarov@gmail.com>
2024-11-25 11:23:52 +0500

    feat: LengthOfLastWord

---
+++ LengthOfLastWord /Program.cs
@@ -0,0 +1,29 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(LengthOfLastWord("Hello World"));
+        Console.WriteLine(LengthOfLastWord("   fly me   to   the moon  "));
+        Console.WriteLine(LengthOfLastWord("luffy is still joyboy"));
+    }
+    
+    private static int LengthOfLastWord(string s)
+    {
+        var length = 0;
+        for (var i = s.Length - 1; i >= 0; i--)
+        {
+            if (s[i] == ' ' && length > 0)
+            {
+                return length;
+            }
+            if (s[i] != ' ')
+            {
+                length++;
+            }
+        }
+        return length;
+        
+        //two variants
+        //return s.Trim().Split(' ').Last().Length;
+    }
+}
