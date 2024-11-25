e2fb9b3e9c67c186c6fd98ac4c353ede40742a78
dilboz <dilbozsafarov@gmail.com>
2024-11-25 11:03:12 +0500

    feat: concatination of array

--- ConcatenationOfArray/Program.cs
+++ ConcatenationOfArray/Program.cs
@@ -9,7 +9,7 @@
     private static int[] ConcatenationOfArray(int[] nums)
     {
         var result = new int[nums.Length * 2];
-        for (int i = 0; i < nums.Length; i++)
+        for (var i = 0; i < nums.Length; i++)
         {
             result[i] = nums[i];
             result[i + nums.Length] = nums[i];
