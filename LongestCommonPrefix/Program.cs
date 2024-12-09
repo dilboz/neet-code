f51ee088a211fb7f9ccb69d1d30666ec2cab4f17
dilboz <dilbozsafarov@gmail.com>
2024-12-09 15:41:14 +0500

    feat: longest  common prefix

---
+++ LongestCommonPrefix/Program.cs
@@ -0,0 +1,31 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(LongestCommonPrefix(["flower", "flow", "flight"]));
+        Console.WriteLine(LongestCommonPrefix(["dog", "racecar", "car"]));
+    }
+
+    private static string LongestCommonPrefix(string[] strs)
+    {
+        if (strs.Length == 0)
+        {
+            return "";
+        }
+        
+        var prefix = strs[0];
+        for (var i = 1; i < strs.Length; i++)
+        {
+            while (strs[i].IndexOf(prefix) != 0)
+            {
+                prefix = prefix.Substring(0, prefix.Length - 1);
+                if (prefix.Length == 0)
+                {
+                    return "";
+                }
+            }
+        }
+        
+        return prefix;
+    }
+}
