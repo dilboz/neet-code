af90189bfdd24dc24908788df220660bbfc1b52a
dilboz <dilbozsafarov@gmail.com>
2024-12-09 15:56:10 +0500

    feat: generate

---
+++ Generate/Program.cs
@@ -0,0 +1,35 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        var result = Generate(5);
+        foreach (var list in result)
+        {
+            Console.WriteLine(string.Join(",", list));
+        }
+    }
+
+    private static IList<IList<int>> Generate(int numRows)
+    {
+        var result = new List<IList<int>>();
+        if (numRows == 0)
+        {
+            return result;
+        }
+
+        result.Add(new List<int> {1});
+        for (var i = 1; i < numRows; i++)
+        {
+            var prev = result[i - 1];
+            var current = new List<int> {1};
+            for (var j = 1; j < prev.Count; j++)
+            {
+                current.Add(prev[j - 1] + prev[j]);
+            }
+            current.Add(1);
+            result.Add(current);
+        }
+
+        return result;
+    }
+}
